6c60bce01d8386640bd16da753e5d263e8d7758f
Utz <samuedut@f8ffc240ec6e.ant.amazon.com>
2022-11-02 14:44:00 -0400

    Steps 1-3

--- src/TransitCalculator.java
+++ src/TransitCalculator.java
@@ -1,4 +1,15 @@
 public class TransitCalculator {
+    //Attributes
+    int days;
+    int rides;
+
+    //Constructor
+    public TransitCalculator(days, rides){
+        this.days = days;
+        this.rides = rides;
+    }
+
+    //main method
     public static void main(String[] args){
 
     }
