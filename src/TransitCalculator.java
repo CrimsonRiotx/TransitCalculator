84daf34866d3c8e6bc245787be996a05ac8803fd
Sam Utz <sam_utz@yahoo.com>
2022-11-02 23:51:45 -0400

    beautify/cleanup of redundant code

--- src/TransitCalculator.java
+++ src/TransitCalculator.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class TransitCalculator {
     //Attributes
     int days;
     int rides;
 
     //Constructor
-    public TransitCalculator(int days, int rides){
+    public TransitCalculator(int days, int rides) {
         this.days = days;
         this.rides = rides;
     }
@@ -13,42 +14,41 @@ public class TransitCalculator {
     String[] NYCFareOptions = {"Pay-per-Ride", "7-day Unlimited Rides", "30-day Unlimited Rides"};
     double[] NYCFarePrices = {2.75, 33.00, 127.00};
 
-    public double unlimited7Price(){
+    public double unlimited7Price() {
         double numberOfWeeks;
-        if(days%7!=0){
-            numberOfWeeks = (days/7) + 1;
-        } else numberOfWeeks = (days/7);
-        double totalCost = numberOfWeeks*NYCFarePrices[1];
-        double pricePerRide = totalCost / rides;
-        return pricePerRide;
+        if (days % 7 != 0) {
+            numberOfWeeks = (days / 7) + 1;
+        } else numberOfWeeks = (days / 7);
+        double totalCost = numberOfWeeks * NYCFarePrices[1];
+        return totalCost / rides;
     }
 
-    public double[] getRidePrices(){
+    public double[] getRidePrices() {
         //creating array for ridePrices
         double[] ridePrices = new double[3];
         double dailyPrice = 2.75;
         ridePrices[0] = dailyPrice;
         ridePrices[1] = unlimited7Price();
-        double monthlyPrice = NYCFarePrices[2]/rides;
+        double monthlyPrice = NYCFarePrices[2] / rides;
         ridePrices[2] = monthlyPrice;
         return ridePrices;
     }
 
-    public String getBestFare(){
-        String bestFare = "";
+    public String getBestFare() {
         double[] ridePrices = getRidePrices();
-            if(ridePrices[0] < ridePrices[1] && ridePrices [0] < ridePrices[2]){
-                bestFare = ("You should get the " + NYCFareOptions[0] + " option at $" + ridePrices[0] + " per ride.");
-            } else if(ridePrices[1] < ridePrices[2] && ridePrices [1] < ridePrices[0]){
-                bestFare = ("You should get the " + NYCFareOptions[1] + " option at $" + ridePrices[1] + " per ride.");
-            } else{
-                bestFare = ("You should get the " + NYCFareOptions[2] + " option at $" + ridePrices[2] + " per ride.");
-            }
+        String bestFare;
+        if (ridePrices[0] < ridePrices[1] && ridePrices[0] < ridePrices[2]) {
+            bestFare = ("You should get the " + NYCFareOptions[0] + " option at $" + ridePrices[0] + " per ride.");
+        } else if (ridePrices[1] < ridePrices[2] && ridePrices[1] < ridePrices[0]) {
+            bestFare = ("You should get the " + NYCFareOptions[1] + " option at $" + ridePrices[1] + " per ride.");
+        } else {
+            bestFare = ("You should get the " + NYCFareOptions[2] + " option at $" + ridePrices[2] + " per ride.");
+        }
         return bestFare;
     }
 
     //main method
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the total number of days you are visiting NYC:");
         int days = input.nextInt();
